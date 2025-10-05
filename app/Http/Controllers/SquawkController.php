00a9024e651b21201b06fb58dd1b34e01bec1338
Jesse Fender <jessefender@jessefender.com>
2025-10-05 14:33:30 -0500

    Realized edit and create may use the same partial, but the call cant be the same due without a lot of extra coding

--- app/Http/Controllers/SquawkController.php
+++ app/Http/Controllers/SquawkController.php
@@ -3,21 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \Illuminate\Contracts\View\Factory;
+use \Illuminate\View\View;
 
 class SquawkController extends Controller
 {
 
-    public function index()
+    public function index(): Factory|View
     {
         return view('home');
     }
-    public function edit()
+
+    public function edit(): Factory|View
     {
-        // shows edit / create view
+        // shows edit form
+        return view('');
     }
 
-    public function list()
+    public function create(): Factory|View
+    {
+        // shows create form
+        return view('');
+    }
+
+    public function list(): Factory|View
     {
         // displays a list of all squawks
+        return view('');
     }
 }
