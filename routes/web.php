31b9121f5b0e97ccf01602992942e4ee11c759aa
Jesse Fender <jessefender@jessefender.com>
2025-10-05 14:23:55 -0500

    Now using the preliminary squawk controller in the routes/web.php file.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SquawkController;
 
 //Route::get('/', function () {
 //    return view('welcome');
 //});
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [SquawkController::class, 'index']);
