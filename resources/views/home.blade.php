82d08ab92f650d77756c8b8e40fabc8fd48d6248
Jesse Fender <jessefender@jessefender.com>
2025-10-05 16:16:17 -0500

    updated the squawk display section to show the squawks in the latest order, and it's pretty cool some of the helper methods provided.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,18 @@
     </x-slot:title>
     <div class="max-w-2xl mx-auto">
         <h1 class="text-3xl font-bold underline">SQUAWKS!</h1>
-        @foreach($chirps as $chirp)
+        @forelse($squawks as $squawk)
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
-                        <div class="font-semibold">{{ $chirp['author'] }}</div>
-                        <div class="mt-1">{{ $chirp['message'] }}</div>
-                        <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
+                        <div class="font-semibold">{{ $squawk->user ? $squawk->user->name : 'Anonymous' }}</div>
+                        <div class="mt-1">{{ $squawk->message }}</div>
+                        <div class="text-sm text-gray-500 mt-2">{{ $squawk->created_at->diffForHumans() }}</div>
                     </div>
                 </div>
             </div>
-        @endforeach
+        @empty
+            <p class="text-grat-500">No squawks yet. Be the first to squawk!</p>
+        @endforelse
     </div>
 </x-layout>
