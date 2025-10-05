cea46be281e12c07fafa62b34b644dbaaa848362
Jesse Fender <jessefender@jessefender.com>
2025-10-05 14:36:11 -0500

    added some dummy data that would normally be pulled from a database, to be displayed as a list.

--- app/Http/Controllers/SquawkController.php
+++ app/Http/Controllers/SquawkController.php
@@ -11,7 +11,24 @@ class SquawkController extends Controller
 
     public function index(): Factory|View
     {
-        return view('home');
+        $chirps = [
+            [
+                'author' => 'Jane Doe',
+                'message' => 'Just deployed my first Laravel app! 🚀',
+                'time' => '5 minutes ago'
+            ],
+            [
+                'author' => 'John Smith',
+                'message' => 'Laravel makes web development fun again!',
+                'time' => '1 hour ago'
+            ],
+            [
+                'author' => 'Alice Johnson',
+                'message' => 'Working on something cool with Chirper...',
+                'time' => '3 hours ago'
+            ]
+        ];
+        return view('home', compact('chirps'));
     }
 
     public function edit(): Factory|View
