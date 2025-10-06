cc8b5def1ff7a27028f14d6ed199a2fe01925610
Jesse Fender <jessefender@jessefender.com>
2025-10-06 18:22:08 -0500

    Forgot to remove the nullable column on the foreign key id.

--- database/migrations/2025_10_05_200140_create_squawks_table.php
+++ database/migrations/2025_10_05_200140_create_squawks_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('squawks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->string('message', 500);
             $table->timestamps();
         });
