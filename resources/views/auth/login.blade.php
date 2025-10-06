b41e816d541ad9fcd437d27027cd3d81c97dcb02
Jesse Fender <jessefender@jessefender.com>
2025-10-05 20:43:27 -0500

    A basic page because it was throwing errors because I had not created the view

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <x-slot:title>
+        Login
+    </x-slot:title>
+
+    <h1 class="font-bold text-4xl">Sorry we are still working on the part of the project</h1>
+    <h3 class="text-3xl text-blue-950 underline cursor-pointer">
+        <a href="/">Click here to navigate to the home page</a>
+    </h3>
+
+</x-layout>
