223d25000389a878f83422ed6a45af76b67f2cd1
Jesse Fender <jessefender@jessefender.com>
2025-10-05 20:22:33 -0500

    Added the login and logout routes to the routes file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\Auth\Logout;
+    use App\Http\Controllers\Auth\Register;
+    use App\Http\Controllers\Auth\Login;
+    use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SquawkController;
 
 //Route::get('/', function () {
@@ -11,9 +14,8 @@ use App\Http\Controllers\SquawkController;
 Route::get('/', [SquawkController::class, 'index']);
 // load the squawk in to the ui to allow editing it
 Route::get('/squawks/{squawk}/edit', [SquawkController::class, 'edit']);
-Route::get('/register', function () {
-    return view('auth.register');
-});
+// display the registration form only when the user is not authenticated
+Route::view('/register', 'auth.register')->middleware('guest')->name('register');
 
 // create a squawk
 Route::post('/squawks', [SquawkController::class, 'store']);
@@ -21,3 +23,10 @@ Route::post('/squawks', [SquawkController::class, 'store']);
 Route::put('/squawks/{squawk}', [SquawkController::class, 'update']);
 // delete database entry for squawk
 Route::delete('/squawks/{squawk}', [SquawkController::class, 'destroy']);
+
+// create a new user in the database and log them in
+Route::post('/register', Register::class)->middleware('guest');
+// log in user when they are guest
+Route::post('/login', Login::class)->middleware('guest');
+// log out a logged-in user only when they are logged-in.
+Route::post('/logout', Logout::class)->middleware('auth');
